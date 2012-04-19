33d0c03ee1fca6eed2ca9e2813aa466fdfc09436
HumbleLivelyBear2 <jackmodo2012@gmail.com>
2012-04-19 16:43:14 +0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JakmodoPivotApp::Application.config.secret_token = '468881edaa9b7e76d16009acb9d643df20f53b531124bf764b28e6280fb9933db6d4ff4da7d2fe283302f1c0e745312a594e9591f0bdfc12d6dc87f7752de293'
